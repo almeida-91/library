3f5333d08d33876499f66bd0f5cf4ca1543a2f47
Almeida-91 <aplalmeida91@outlook.com>
2022-08-07 00:27:20 +0100

    Changed to class implementation

--- script.js
+++ script.js
@@ -8,69 +8,76 @@ const delButtonList = document.getElementsByClassName('delButton');
 let myLibrary = []; 
 let id=0;
 
-function book(title,author, pages, read){
+class book{
+constructor (title,author, pages, read){
     this.author = author
     this.title = title
     this.pages = pages
     this.read = read
-    this.toggle = function(){
+    myLibrary.push(this);
+    this.refreshLibrary();
+    }
+
+    toggle(){
         if (this.read==`Yes`) this.read = 'No';
         else this.read = 'Yes';
-        refreshLibrary();
+        this.refreshLibrary();
     }
-}
 
-function addBookToLibrary(title,author,pages,read) {
-    let newbook = new book;
-    newbook.title = title;
-    newbook.author = author;
-    newbook.pages = pages;
-    newbook.read = read;
-    console.log(newbook);
-    myLibrary.push(newbook);
-    refreshLibrary();
-}
+    deleteBook(id){
+        myLibrary.splice(id, 1);
+        this.refreshLibrary();
+    }
 
-addBookToLibrary('The Hobbit','JRR Tolkien',295,'No');
-addBookToLibrary('HP','rowling',295,'Yes');
-addBookToLibrary(`Troglo's thesis`,`Troglo Pedreira`,`9000`,'Yes');
+    changeRead(index){
+        if (myLibrary[index].read == 'No') myLibrary[index].read = 'Yes';
+        else myLibrary[index].read = 'No';
+        this.refreshLibrary();
+    }
 
-function refreshLibrary(){
-    booklist.innerHTML = "";
-    let id = 0;
-    for (i=0 ; i < myLibrary.length ; i++){
-        let row = document.createElement('tr');
-        for (j=0 ; j < 7 ; j++){
-            let cell = document.createElement('td');
-            if (j==0) cell.textContent = id;
-            else if (j==1) cell.textContent = myLibrary[i].title;
-            else if (j==2) cell.textContent = myLibrary[i].author;
-            else if (j==3) cell.textContent = myLibrary[i].pages;
-            else if (j==4) cell.textContent = myLibrary[i].read;
-            else if (j==5) {
-                let delBook = document.createElement('button');
-                delBook.className = `delButton`;
-                delBook.textContent = `Delete`;
-                delBook.setAttribute('name',i);
-                cell.appendChild(delBook);
-                delBook.onclick = function (){deleteBook(Number(delBook.getAttribute('name')))};
-            }
-            else if (j==6) {
-                let change = document.createElement('button');
-                change.className = 'statusButton';
-                change.textContent = 'Change';
-                change.setAttribute('name',i);
-                change.onclick = function(){
-                    myLibrary[Number(change.getAttribute('name'))].toggle();
+    refreshLibrary(){
+        booklist.innerHTML = "";
+        let id = 0;
+        for (let i=0 ; i < myLibrary.length ; i++){
+            let row = document.createElement('tr');
+            for (let j=0 ; j < 7 ; j++){
+                let cell = document.createElement('td');
+                if (j==0) cell.textContent = id;
+                else if (j==1) cell.textContent = myLibrary[i].title;
+                else if (j==2) cell.textContent = myLibrary[i].author;
+                else if (j==3) cell.textContent = myLibrary[i].pages;
+                else if (j==4) cell.textContent = myLibrary[i].read;
+                else if (j==5) {
+                    let delBook = document.createElement('button');
+                    delBook.className = `delButton`;
+                    delBook.textContent = `Delete`;
+                    delBook.setAttribute('name',i);
+                    cell.appendChild(delBook);
+                    delBook.onclick = function (){myLibrary[delBook.getAttribute('name')].deleteBook(delBook.getAttribute('name'))};
                 }
-                cell.appendChild(change);  
+                else if (j==6) {
+                    let change = document.createElement('button');
+                    change.className = 'statusButton';
+                    change.textContent = 'Change';
+                    change.setAttribute('name',i);
+                    change.onclick = function(){
+                        myLibrary[Number(change.getAttribute('name'))].toggle();
+                    }
+                    cell.appendChild(change);  
+                }
+                row.appendChild(cell);
             }
-            row.appendChild(cell);
+            id++;
+            booklist.appendChild(row);
         }
-        id++;
-        booklist.appendChild(row);
     }
 }
+
+new book('The Hobbit','JRR Tolkien',295,'No');
+new book('HP','rowling',295,'Yes');
+new book(`Troglo's thesis`,`Troglo Pedreira`,`9000`,'Yes');
+
+
 
 const newBookForm = document.getElementById(`newBookForm`);
 
@@ -81,13 +88,8 @@ newBook.addEventListener(`click`, ()=>{
 });
 
 addBook.addEventListener('click', function(){
-    addBookToLibrary(input[0].value,input[1].value,input[2].value,input[3].value);
+   new book(input[0].value,input[1].value,input[2].value,input[3].value);
 });
-
-function deleteBook(id){
-    myLibrary.splice(id, 1);
-    refreshLibrary();
-}
 
 function updateChangeButtons(){
     const changeButtonList = document.getElementsByClassName('statusButton');
@@ -102,8 +104,3 @@ function updateChangeButtons(){
     console.log(changeButtonList);
 }
 
-function changeRead(index){
-    if (myLibrary[index].read == 'No') myLibrary[index].read = 'Yes';
-    else myLibrary[index].read = 'No';
-    refreshLibrary();
-}
